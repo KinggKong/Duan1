99963ef279b0e5b82f3f68de639b51c091ad2162
KinggKong <134825851+KinggKong@users.noreply.github.com>
2023-11-12 19:18:49 +0700

    update

--- src/com/thuvien/entity/NXB.java
+++ src/com/thuvien/entity/NXB.java
@@ -1,7 +1,13 @@
 package com.thuvien.entity;
 
 public class NXB {
+	private int id;
 	private String tenNXB;
+
+	public NXB(int id, String tenNXB) {
+		this.id = id;
+		this.tenNXB = tenNXB;
+	}
 
 	public NXB(String tenNXB) {
 		this.tenNXB = tenNXB;
@@ -16,6 +22,14 @@ public class NXB {
 
 	public void setTenNXB(String tenNXB) {
 		this.tenNXB = tenNXB;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
