57389b36230c3145a21a5899cccff63f464e0032
kolagi06 <149390180+kolagi06@users.noreply.github.com>
2023-11-17 19:26:49 +0700

    Sửa nhân vien

---
+++ src/com/thuvien/dao/PhieuMuonDao.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.thuvien.dao;
+
+import com.thuvien.entity.PhieuMuon;
+import com.thuvien.entity.QuyenSach;
+import com.thuvien.utils.JDBCHelper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tbluc
+ */
+public class PhieuMuonDao extends QLTVDao<PhieuMuon, String>{
+  
+}
